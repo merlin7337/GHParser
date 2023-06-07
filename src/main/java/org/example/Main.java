ed4691fa563192dbd09793acc84d02e08fd4616b
Fatum73 <112899660+Fatum73@users.noreply.github.com>
2023-06-07 20:55:27 +0300

    Added basic GitHub achievements parser logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Elements achievements = parseAchievements("torvalds");
+            for (Element e : achievements) {
+                System.out.println(e.text());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static Elements parseAchievements(String nickname) throws IOException {
+        Document doc = Jsoup.connect("https://github.com/" + nickname + "?tab=achievements").get();
+        return doc.getElementsByClass("d-flex flex-justify-center mb-1");
     }
 }
