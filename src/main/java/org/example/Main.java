34d56e0249b8a8dfedcb747f9bcdc6c6975d1e9c
Fatum73 <112899660+Fatum73@users.noreply.github.com>
2023-06-12 10:16:38 +0300

    Added user public repositories parsing, repository data parsing, user activity parsing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         String nickname = "torvalds";
         try {
-            Map<String, String> userAchievements = Parser.parseUserAchievements(nickname);
             Map<String, String> userInfo = Parser.parseUserInfo(nickname);
             System.out.println("User " + nickname + " info:");
             for (Map.Entry<String, String> entry : userInfo.entrySet()) {
@@ -15,11 +15,32 @@ public class Main {
                 String value = entry.getValue();
                 System.out.println("    " + key + " : " + value);
             }
+
+            Map<String, String> userAchievements = Parser.parseUserAchievements(nickname);
             System.out.println("User " + nickname + " achievements:");
             for (Map.Entry<String, String> entry : userAchievements.entrySet()) {
                 String key = entry.getKey();
                 String value = entry.getValue();
                 System.out.println("    " + key + " : " + value);
+            }
+
+            Map<String, String> userActivity = Parser.parseUserActivity(nickname);
+            System.out.println("User " + nickname + " activity:");
+            for (Map.Entry<String, String> entry : userActivity.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println("    " + key + " : " + value);
+            }
+
+            List<Map<String, String>> userPublicRepositories = Parser.parseUserPublicRepositories(nickname);
+            System.out.println("User " + nickname + " repositories:");
+            for (Map<String, String> e : userPublicRepositories) {
+                System.out.println("    " + e.get("title") + ":");
+                for (Map.Entry<String, String> entry : e.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    System.out.println("        " + key + " : " + value);
+                }
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -6,15 +6,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
     public static Map<String, String> parseUserAchievements(String nickname) throws IOException {
         Document doc = Jsoup.connect("https://github.com/" + nickname + "?tab=achievements").get();
         Map<String, String> userAchievements = new HashMap<>();
-        Elements achievements = doc.getElementsByClass("d-flex flex-justify-center mb-1");
 
+        Elements achievements = doc.getElementsByClass("d-flex flex-justify-center mb-1");
         for (Element e : achievements) {
             if (e.text().contains("x")) {
                 userAchievements.put(e.text().substring(0, e.text().lastIndexOf('x')).trim(),
@@ -41,12 +43,56 @@ public class Parser {
         userInfo.put("home location", homeLocation);
 
         String followersAndFollowing = doc.select("span[class=\"text-bold color-fg-default\"]").text();
-        String followers = followersAndFollowing.substring(0, followersAndFollowing.indexOf(' '));
+
+        String followers = followersAndFollowing.substring(0, followersAndFollowing.indexOf(' '))
+                .replace("k", "000");
         userInfo.put("followers", followers);
-        String following = followersAndFollowing.substring(followersAndFollowing.indexOf(' ')).trim();
+
+        String following = followersAndFollowing.substring(followersAndFollowing.indexOf(' ')).trim()
+                .replace("k", "000");
         userInfo.put("following", following);
 
         return userInfo;
     }
 
+    public static Map<String, String> parseUserActivity(String nickname) throws IOException {
+        Document doc = Jsoup.connect("https://github.com/" + nickname).get();
+        Map<String, String> userActivity = new HashMap<>();
+
+        String lastYearContributions = doc.select("h2[class=\"f4 text-normal mb-2\"]").text();
+        Pattern pattern = Pattern.compile("\\d+.\\d+");
+        Matcher matcher = pattern.matcher(lastYearContributions);
+        while (matcher.find()) {
+            lastYearContributions = lastYearContributions.substring(matcher.start(), matcher.end());
+        }
+        userActivity.put("last year contributions", lastYearContributions.replace(",", "").trim());
+
+        return userActivity;
+    }
+
+    public static List<Map<String, String>> parseUserPublicRepositories(String nickname) throws IOException {
+        Document doc = Jsoup.connect("https://github.com/" + nickname + "?tab=repositories").get();
+        Elements repositories = doc.select("div[class=\"col-10 col-lg-9 d-inline-block\"]");
+        List<Map<String, String>> repositoriesList = new ArrayList<>();
+
+        for (Element e : repositories) {
+            repositoriesList.add(parseRepository(nickname, e.select("a[itemprop=\"name codeRepository\"]").text()));
+        }
+        return repositoriesList;
+    }
+
+    public static Map<String, String> parseRepository(String nickname, String repoName) throws IOException {
+        String url = "https://github.com/" + nickname + "/" + repoName;
+        Document doc = Jsoup.connect(url).get();
+        Map<String, String> repo = new HashMap<>();
+        repo.put("title", repoName);
+        repo.put("URL", url);
+        repo.put("description", doc.select("p[class=\"f4 my-3\"]").text());
+        repo.put("commits count", doc.select("li[class=\"ml-0 ml-md-3\"]").text()
+                .replace(" commits", "")
+                .replace(",", ""));
+        repo.put("last edited", doc.select("relative-time").text());
+        repo.put("last commit code", doc.select("a[class=\"f6 Link--secondary text-mono ml-2 d-none d-lg-inline\"]").text());
+        return repo;
+    }
 }
